9fc60ca54e8d8a2934b208ff9004fb1b00f88ffa
Ulysses V <ulysses@hub9.co>
2015-12-02 03:10:36 -0200

    Adding footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $(function() {
+	/* Plays the video when scrolling near the area. */
 	var aboutElement = $('.about-content');
 	var played = false;
-
 	$(window).scroll(function() {
 		if ($(window).scrollTop() + $(window).height() > aboutElement.offset().top + aboutElement.height()) {
 			if (!played) {
@@ -9,5 +9,20 @@ $(function() {
 				played = true;
 			}
 		}
+	});
+
+	/* Displays the map. */
+	var position = new google.maps.LatLng(-22.949734, -43.187119);
+	var mapOptions = {
+		center: position,
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true
+	};
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	var marker = new google.maps.Marker({
+		position: position,
+		map: map,
+		title: "Bunker"
 	});
 });
