d2d74093c9a0b3b8a9b7b5ca219b844277f76385
Ulysses V <ulysses@hub9.co>
2016-03-22 01:30:10 -0300

    Reenabling animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -133,7 +133,7 @@ $(function() {
 	$(window).scroll(function() {
 		if ($(window).scrollTop() + $(window).height() > aboutElement.offset().top + aboutElement.height()) {
 			if (!played) {
-				//$('#video').get(0).play();
+				$('#video').get(0).play();
 				played = true;
 			}
 		}
